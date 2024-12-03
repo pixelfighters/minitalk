7346c51e6a7f6a327c7eabe6f2e9c0462d5d9a04
pixelfightersdev <dev@pixelfighters.com>
2024-12-03 20:47:59 +0100

    repair

--- client.c
+++ client.c
@@ -1,114 +1,89 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuh <fschuh@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kami <kami@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 10:06:50 by kami              #+#    #+#             */
-/*   Updated: 2024/10/09 11:46:12 by fschuh           ###   ########.fr       */
+/*   Updated: 2024/12/03 20:41:03 by kami             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minitalk.h"
 
-int	check_args(int argc, char **argv)
+void	signal_error(void)
 {
-	if (argc != 3)
-	{
-		ft_print_ascii_art("ERROR", KRED, 1);
-		ft_errhandle("Invalid input.\nPlease enter <PID> and Message.");
-		return (0);
-	}
-	if (!ft_isdigit(argv[1][1]))
-	{
-		ft_print_ascii_art("ERROR", KRED, 1);
-		ft_errhandle("Invalid PID.\nPlease enter valid <PID>.");
-		return (0);
-	}
-	if (argv[2] == NULL)
-	{
-		ft_print_ascii_art("ERROR", KRED, 1);
-		ft_errhandle("Invalid Message.\nPlease enter valid Message.");
-		return (0);
-	}
-	return (1);
+	ft_print_ascii_art("ERROR", KRED, 1);
+	ft_errhandle("Unexpected SIG Error.\n");
+	exit(EXIT_FAILURE);
 }
 
-void	send_msg(pid_t sv_pid, char *msg)
+void	convert(unsigned char c, int pid)
 {
-	unsigned char	c;
-	int				nbr_bits;
+	int	bit;
 
-	while (*msg)
+	bit = 0;
+	while (bit < 8)
 	{
-		c = *msg;
-		nbr_bits = 8;
-		while (nbr_bits--)
+		if (c & 128)
 		{
-			if (c & 0b10000000)
-				kill(sv_pid, SIGUSR1);
-			else
-				kill(sv_pid, SIGUSR2);
-			usleep(50);
-			c <<= 1;
+			if (kill(pid, SIGUSR2) == -1)
+				signal_error();
 		}
-		msg++;
-	}
-	c = '\0';
-	nbr_bits = 8;
-	while (nbr_bits--)
-	{
-		if (c & 0b10000000)
-			kill(sv_pid, SIGUSR1);
 		else
-			kill(sv_pid, SIGUSR2);
-		usleep(50);
-		c <<= 1;
-	}
-}
-
-void	signal_actions(int signum)
-{
-	ssize_t	bytes_written;
-
-	if (signum == SIGUSR2)
-	{
-		bytes_written = ft_printf("%sServer received Message.%s\n\n", KGRN, KNRM);
-		if (bytes_written == -1)
 		{
-			ft_errhandle("Error writing to stdout");
+			if (kill(pid, SIGUSR1) == -1)
+				signal_error();
 		}
+		c <<= 1;
+		bit++;
+		pause();
+		usleep(100);
 	}
 }
 
-void	config_signals(void)
+void	sent_text(char *str, int pid)
 {
-    struct sigaction	sa_newsig;
+	int	i;
 
-    ft_memset(&sa_newsig, 0, sizeof(sa_newsig));
-    sa_newsig.sa_handler = signal_actions;
-    // sa_newsig.sa_flags = SA_RESTART; 
-    if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
-        ft_errhandle("Failed to change SIGUSR1's behavior");
-    if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
-        ft_errhandle("Failed to change SIGUSR2's behavior");
+	i = 0;
+	while (str[i])
+		convert(str[i++], pid);
+	convert('\0', pid);
 }
 
+void	success(int sig)
+{
+	static int	sent;
+
+	if (sig == SIGUSR1)
+	{
+		ft_printf("%s%d signals%s sent successfully!%s\n",
+			KGRN, ++sent, KNRM, KNRM);
+		exit(EXIT_SUCCESS);
+	}
+	if (sig == SIGUSR2)
+		++sent;
+}
 
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
 
-	if (check_args(argc, argv))
+	ft_printf("argv1 atoi %d", ft_atoi(argv[1]));
+	if (argc == 3 && (ft_atoi(argv[1]) > 0))
 	{
 		ft_print_ascii_art("TALK CLIENT", KCYN, 1);
 		ft_printf("Sendig %s%s%s to PID: %s%s%s\n",
 			KYEL, argv[2], KNRM, KCYN, argv[1], KNRM);
+		signal(SIGUSR1, success);
+		signal(SIGUSR2, success);
 		server_pid = ft_atoi(argv[1]);
-		config_signals();
-		send_msg(server_pid, argv[2]);
+		ft_printf("%sSending to Server%s\n", KYEL, KNRM);
+		sent_text(argv[2], server_pid);
+		ft_printf("%s", KNRM);
 		return (EXIT_SUCCESS);
 	}
 	else
@@ -116,5 +91,4 @@ int	main(int argc, char **argv)
 		ft_print_ascii_art("Failure", KRED, 1);
 		return (EXIT_FAILURE);
 	}
-	return (0);
 }
--- server.c
+++ server.c
@@ -1,83 +1,86 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   server_2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuh <fschuh@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kami <kami@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 10:06:50 by kami              #+#    #+#             */
-/*   Updated: 2024/10/09 11:45:55 by fschuh           ###   ########.fr       */
+/*   Updated: 2024/12/03 20:41:19 by kami             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include "libft.h"
 
-static int pid_printed;
-static int handler_configured = 0;
- 
-void	handle_sigusr(int sig, siginfo_t *info, void *msg)
+void	signal_error(void)
 {
-	static int				i = -1;
-	static unsigned char	c;
-
-	(void)msg;
-	// ft_printf("pid_printed: %d - info->si_pid: %d\n", pid_printed, info->si_pid);
-	if (pid_printed != info->si_pid && info != NULL && handler_configured <= 1)
-	{
-		ft_printf("Signal from PID %s%d%s\n\n", KCYN, info->si_pid, KNRM);
-		pid_printed = info->si_pid;
-	}
-	if (i < 0)
-		i = 7;
-	if (sig == SIGUSR1)
-		c |= (1 << i);
-	i--;
-    if (i < 0)
-    {
-        if (c == '\0')
-        {
-            ft_printf("\n%sMessage complete.%s\n\n", KGRN, KNRM);
-            if (kill(info->si_pid, SIGUSR2) == -1)
-                ft_errhandle("cannot send SIGUSR2");
-            c = 0;
-            return;
-        }
-        ft_putchar_fd(c, STDOUT_FILENO);
-        c = 0;
-        if (kill(info->si_pid, SIGUSR1) == -1)
-            ft_errhandle("cannot send SIGUSR1");
-        i = 7; 
-    }
+	ft_print_ascii_art("ERROR", KRED, 1);
+	ft_errhandle("Unexpected SIG Error.\n");
+	exit(EXIT_FAILURE);
 }
 
-void	config_signals(void)
+void	extended_action(char *c, int *received, int *client_pid, int *bit)
 {
-	struct sigaction	sa_newsig;
+	ft_printf("%c", *c);
+	if (*c == '\0')
+	{
+		ft_printf("\n%s%d signals%s recieved from client PID: %d%s\n",
+			KGRN, *received, KNRM, *client_pid, KNRM);
+		*received = 0;
+		*c = 0;
+		if (kill(*client_pid, SIGUSR1) == -1)
+			signal_error();
+		return ;
+	}
+	*bit = 0;
+}
 
-	if (handler_configured)
-        return;
-	sa_newsig.sa_sigaction = &handle_sigusr;
-	sa_newsig.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa_newsig.sa_mask);
-	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
-		ft_errhandle("Cannot change SIGUSR1");
-	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
-		ft_errhandle("Cannot change SIGUSR2");
-	handler_configured = 1;
+void	action(int sig, siginfo_t *info, void *context)
+{
+	static pid_t	client_pid;
+	static int		bit;
+	static char		c;
+	static int		received;
+	static pid_t	current_pid;
+
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	current_pid = info->si_pid;
+	if (client_pid != current_pid)
+	{
+		client_pid = current_pid;
+		bit = 0;
+		c = 0;
+		received = 0;
+	}
+	c |= (sig == SIGUSR2);
+	received++;
+	bit++;
+	if (bit == 8)
+		extended_action(&c, &received, &client_pid, &bit);
+	c <<= 1;
+	usleep(100);
+	kill(client_pid, SIGUSR2);
 }
 
 int	main(void)
 {
-	pid_t	pid;
+	pid_t				pid;
+	struct sigaction	act;
 
 	pid = getpid();
+	act.sa_sigaction = action;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
 	ft_print_ascii_art("TALK SERVER", KCYN, 1);
 	ft_printf("SERVER PID = %s%d%s\n\n", KCYN, pid, KNRM);
-    config_signals();
-    while (1)
-    {
-        pause();
-    }
-	return (0);
+	while (1)
+	{
+		sigaction(SIGUSR1, &act, 0);
+		sigaction(SIGUSR2, &act, 0);
+		pause();
+	}
+	return (EXIT_FAILURE);
 }
